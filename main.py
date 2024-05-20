5637841f8e6d03d66bf48d9edbe18dd9602ad738
Guo Te <54687332+dkfzphd@users.noreply.github.com>
2024-05-20 15:07:25 +0200

    Update main.py
    -Reduce string splicing operations: process byte data directly, avoiding frequent encoding and decoding.
    
    -More efficient processing of GPS data: Split data by locating the terminator \r\n the NMEA statement, rather than decoding and then splitting.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uasyncio as asyncio
-from machine import UART,reset,Pin
+from machine import UART, reset, Pin
 import bluetooth
 from ble_uart import BLEUART
 
@@ -10,15 +10,17 @@ uart_ble = BLEUART(ble, name="GNSS-Receiver")
 # Initialize GPS UART
 uart_gps = UART(1, baudrate=115200, tx=0, rx=1, timeout=10)  # Modify these settings based on your hardware 
 
-# Initialize LED
-led13 = Pin(13, Pin.OUT)
-led12 = Pin(12, Pin.OUT)
+# Initialize LEDs
+led_data = Pin(13, Pin.OUT)
+led_error = Pin(12, Pin.OUT)
+
 # Send GPS data to BLE device
 def send_gps_data(data):
     try:
         uart_ble.write(data)
     except Exception as e:
         print("Error sending GPS data:", str(e))
+        led_error.on()
 
 # Define an async function to read and forward GPS data
 async def forward_gps_data():
@@ -26,27 +28,31 @@ async def forward_gps_data():
     while True:
         try:
             gps_data = uart_gps.read(256)
-            led13.off()
             if gps_data:
                 gps_buffer += gps_data
-                decoded_data = gps_buffer.decode('utf-8')
-                sentences = decoded_data.split('\r\n')
-                for sentence in sentences[:-1]:
-                    if sentence.startswith('$'):
-                        print(sentence)
-                        send_gps_data(sentence.encode('utf-8') + b'\r\n')  # Send complete NMEA sentence
-                        led13.on()
-                gps_buffer = sentences[-1].encode('utf-8')
-                
+                while b'\r\n' in gps_buffer:
+                    sentence, gps_buffer = gps_buffer.split(b'\r\n', 1)
+                    if sentence.startswith(b'$'):
+                        print(sentence.decode('utf-8'))
+                        led_data.on()
+                        send_gps_data(sentence + b'\r\n')  # Send complete NMEA sentence
+                        led_data.off()
+
             await asyncio.sleep(0.1)  # Send data once per 0.1 second
 
+        except OSError as e:
+            print("UART read error:", str(e))
+            led_error.on()
+            await asyncio.sleep(1)
+            reset()
         except Exception as e:
             print("Exception occurred:", str(e))
+            led_error.on()
+            await asyncio.sleep(1)
             reset()
 
 # Create an event loop and run the GPS data forwarding task
 loop = asyncio.get_event_loop()
 loop.create_task(forward_gps_data())
 loop.run_forever()
-
 
